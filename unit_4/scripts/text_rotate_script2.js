ed27fb3fc0b062009b5d4e25f218b6a87de8147f
Zachary Stiefler <zstiefler@gmail.com>
2015-10-26 16:16:28 -0400

    v3

--- unit_4/scripts/text_rotate_script2.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    $.typer.options.tapeColor = 'rgba(0,0,0,.35)';
-    $.typer.options.textColor = 'rgb(255,255,255)';
-    $.typer.options.highlightSpeed = 40;
-    $.typer.options.typeSpeed = 100;
-    $.typer.options.typerInterval = 800;
-    $.typer.options.initialDelay = 1800;
-    $('[data-typer-targets]').typer();
-})(jQuery);
